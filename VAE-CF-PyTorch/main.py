9c04e4c16a2fa6025e2914ccd773551bb050c184
James Le <le_j6@denison.edu>
2019-10-01 16:12:39 -0400

    Train and Evaluate the Model

--- VAE-CF-PyTorch/main.py
+++ VAE-CF-PyTorch/main.py
@@ -67,3 +67,165 @@ model = models.MultiVAE(p_dims).to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=args.wd)
 criterion = models.loss_function
+
+###############################################################################
+# Training code
+###############################################################################
+
+# TensorboardX Writer
+
+writer = SummaryWriter()
+
+def sparse2torch_sparse(data):
+    """
+    Convert scipy sparse matrix to torch sparse tensor with L2 Normalization
+    This is much faster than naive use of torch.FloatTensor(data.toarray())
+    https://discuss.pytorch.org/t/sparse-tensor-use-cases/22047/2
+    """
+    samples = data.shape[0]
+    features = data.shape[1]
+    coo_data = data.tocoo()
+    indices = torch.LongTensor([coo_data.row, coo_data.col])
+    row_norms_inv = 1 / np.sqrt(data.sum(1))
+    row2val = {i : row_norms_inv[i].item() for i in range(samples)}
+    values = np.array([row2val[r] for r in coo_data.row])
+    t = torch.sparse.FloatTensor(indices, torch.from_numpy(values).float(), [samples, features])
+    return t
+
+def naive_sparse2tensor(data):
+    return torch.FloatTensor(data.toarray())
+
+def train():
+    # Turn on training mode
+    model.train()
+    train_loss = 0.0
+    start_time = time.time()
+    global update_count
+
+    np.random.shuffle(idxlist)
+
+    for batch_idx, start_idx in enumerate(range(0, N, args.batch_size)):
+        end_idx = min(start_idx + args.batch_size, N)
+        data = train_data[idxlist[start_idx:end_idx]]
+        data = naive_sparse2tensor(data).to(device)
+
+        if args.total_anneal_steps > 0:
+            anneal = min(args.anneal_cap, 1. * update_count / args.total_anneal_steps)
+        else:
+            anneal = args.anneal_cap
+
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+
+        loss = criterion(recon_batch, data, mu, logvar, anneal)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+
+        update_count += 1
+
+        if batch_idx % args.log_interval == 0 and batch_idx > 0:
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:4d}/{:4d} batches | ms/batch {:4.2f} | '
+                    'loss {:4.2f}'.format(
+                        epoch, batch_idx, len(range(0, N, args.batch_size)),
+                        elapsed * 1000 / args.log_interval,
+                        train_loss / args.log_interval))
+
+            # Log loss to tensorboard
+            n_iter = (epoch - 1) * len(range(0, N, args.batch_size)) + batch_idx
+            writer.add_scalars('data/loss', {'train': train_loss / args.log_interval}, n_iter)
+
+            start_time = time.time()
+            train_loss = 0.0
+
+def evaluate(data_tr, data_te):
+    # Turn on evaluation mode
+    model.eval()
+    total_loss = 0.0
+    global update_count
+    e_idxlist = list(range(data_tr.shape[0]))
+    e_N = data_tr.shape[0]
+    n100_list = []
+    r20_list = []
+    r50_list = []
+
+    with torch.no_grad():
+        for start_idx in range(0, e_N, args.batch_size):
+            end_idx = min(start_idx + args.batch_size, N)
+            data = data_tr[e_idxlist[start_idx:end_idx]]
+            heldout_data = data_te[e_idxlist[start_idx:end_idx]]
+
+            data_tensor = naive_sparse2tensor(data).to(device)
+
+            if args.total_anneal_steps > 0:
+                anneal = min(args.anneal_cap, 1. * update_count / args.total_anneal_steps)
+            else:
+                anneal = args.anneal_cap
+
+            recon_batch, mu, logvar = model(data_tensor)
+
+            loss = criterion(recon_batch, data_tensor, mu, logvar, anneal)
+            total_loss += loss.item()
+
+            # Exclude examples from training set
+            recon_batch = recon_batch.cpu().numpy()
+            recon_batch[data.nonzero()] = -np.inf
+
+            n100 = metric.NDCG_binary_at_k_batch(recon_batch, heldout_data, 100)
+            r20 = metric.Recall_at_k_batch(recon_batch, heldout_data, 20)
+            r50 = metric.Recall_at_k_batch(recon_batch, heldout_data, 50)
+
+            n100_list.append(n100)
+            r20_list.append(r20)
+            r50_list.append(r50)
+
+    total_loss /= len(range(0, e_N, args.batch_size))
+    n100_list = np.concatenate(n100_list)
+    r20_list = np.concatenate(r20_list)
+    r50_list = np.concatenate(r50_list)
+
+    return total_loss, np.mean(n100_list), np.mean(r20_list), np.mean(r50_list)
+
+best_n100 = -np.inf
+update_count = 0
+
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+    for epoch in range(1, args.epochs + 1):
+        epoch_start_time = time.time()
+        train()
+        val_loss, n100, r20, r50 = evaluate(vad_data_tr, vad_data_te)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:4.2f}s | valid loss {:4.2f} | '
+                'n100 {:5.3f} | r20 {:5.3f} | r50 {:5.3f}'.format(
+                    epoch, time.time() - epoch_start_time, val_loss,
+                    n100, r20, r50))
+        print('-' * 89)
+
+        n_iter = epoch * len(range(0, N, args.batch_size))
+        writer.add_scalars('data/loss', {'valid': val_loss}, n_iter)
+        writer.add_scalar('data/n100', n100, n_iter)
+        writer.add_scalar('data/r20', r20, n_iter)
+        writer.add_scalar('data/r50', r50, n_iter)
+
+        # Save the model if the n100 is the best we've seen so far.
+        if n100 > best_n100:
+            with open(args.save, 'wb') as f:
+                torch.save(model, f)
+            best_n100 = n100
+
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
+
+# Load the best saved model.
+with open(args.save, 'rb') as f:
+    model = torch.load(f)
+
+# Run on test data.
+test_loss, n100, r20, r50 = evaluate(test_data_tr, test_data_te)
+print('=' * 89)
+print('| End of training | test loss {:4.2f} | n100 {:4.2f} | r20 {:4.2f} | '
+        'r50 {:4.2f}'.format(test_loss, n100, r20, r50))
+print('=' * 89)
