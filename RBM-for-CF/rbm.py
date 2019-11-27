8d2b49dc308fa6e02996ae0d296e37ef60d4f02a
James Le <le_j6@denison.edu>
2019-11-27 11:37:18 -0500

    Update the RBM Implementation

--- RBM-for-CF/rbm.py
+++ RBM-for-CF/rbm.py
@@ -1,125 +1,75 @@
 import torch
-from engine import Engine
-from utils import use_cuda
-from utils import resume_checkpoint
 
-class RBM:
-	'''
-	Implementation of the Restricted Boltzmann Machine for collaborative filtering. The model is based on the paper of Ruslan Salakhutdinov, Andriy Mnih and Geoffrey Hinton: https://www.cs.toronto.edu/~rsalakhu/papers/rbmcf.pdf
-	'''
+# Create the architecture of the Restricted Boltzmann Machine
+class RBM():
+    def __init__(self, n_vis, n_hid):
 
-	def __init__(self, num_visible, num_hidden, k, momentum_coefficient=0.5, config):
-		'''Initialization of the model  '''
-		super(RBM, self).__init__()
+        '''
+        Initialize the parameters (weights and biases) we optimize during the training process
+        @self: the class object
+        @n_vis: number of visible units
+        @n_hid: number of hidden units
+        '''
 
-		self.config = config
-		self.num_users = config['num_users']
-		self.num_items = config['num_items']
+        # Weights used for the probability of the visible units given the hidden units
+        self.W = torch.randn(n_hid, n_vis) # torch.rand: random normal distribution mean = 0, variance = 1
 
-		self.num_visible = config['num_visible']
-		self.num_hidden = config['num_hidden']
-		self.k = config['k']
-		self.momentum_coefficient = config['momentum_coefficient']
+        # Bias probability of the visible units is activated, given the value of the hidden units (p_v_given_h)
+        self.v_bias = torch.randn(1, n_vis) # fake dimension for the batch = 1
 
-		self.use_cuda = use_cuda
+        # Bias probability of the hidden units is activated, given the value of the visible units (p_h_given_v)
+        self.h_bias = torch.randn(1,n_hid) # fake dimension for the batch = 1
 
-		self.weights = torch.randn(num_visible, num_hidden) * 0.1
-		self.visible_bias = torch.ones(num_visible) * 0.5
-		self.hidden_bias = torch.zeros(num_hidden)
+    def sample_h(self, x):
 
-		self.weights_momentum = torch.zeros(num_visible, num_hidden)
-		self.visible_bias_momentum = torch.zeros(num_visible)
-		self.hidden_bias_momentum = torch.zeros(num_hidden)
+        '''
+        Sample the hidden units
+        '''
 
-		if self.use_cuda:
-			self.weights = self.weights.cuda()
-			self.visible_bias = self.visible_bias.cuda()
-			self.hidden_bias = self.hidden_bias.cuda()
+        # Probability h is activated given that the value v is sigmoid(Wx + a)
+        # torch.mm make the product of 2 tensors
+        # W.t() take the transpose because W is used for the p_v_given_h
+        wx = torch.mm(x, self.W.t())
 
-			self.weights_momentum = self.weights_momentum.cuda()
-			self.visible_bias_momentum = self.visible_bias_momentum.cuda()
-			self.hidden_bias_momentum = self.hidden_bias_momentum.cuda()
+        # Expand the mini-batch
+        activation = wx + self.h_bias.expand_as(wx)
 
-	def sample_hidden(self, visible_probabilities):
-		'''Uses the visible nodes for calculation of the probabilities that a hidden neuron is activated.'''
+        # Calculate the probability p_h_given_v
+        p_h_given_v = torch.sigmoid(activation)
 
-		hidden_activations = torch.matmul(visible_probabilities, self.weights) + self.hidden_bias
-		hidden_probabilities = self._sigmoid(hidden_activations)
-		return hidden_probabilities
+        # Bernouilli RBM to predict whether an user loves the movie or not (0 or 1)
+        # This correspnds to whether the n_hid is activated or not activated
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
 
-	def sample_visible(self, hidden_probabilities):
-		'''Uses the hidden nodes for calculation of  the probabilities that a visible neuron is activated.'''
+    def sample_v(self, y):
 
-		visible_activations = torch.matmul(hidden_probabilities, self.weights.t()) + self.visible_bias
-		visible_probabilities = self._sigmoid(visible_activations)
-		return visible_probabilities
+        '''
+        Sample the visible units
+        '''
 
-	def contrastive_divergence(self, input_data):
-		''' Computing the gradients of the weights and bias terms with Contrastive Divergence.'''
+        # Probability v is activated given that the value h is sigmoid(Wx + a)
+        wy = torch.mm(y, self.W)
 
-		# Positive phase
-		positive_hidden_probabilities = self.sample_hidden(input_data)
-		positive_hidden_activations = (positive_hidden_probabilities >= self._random_probabilities(self.num_hidden)).float()
-		positive_associations = torch.matmul(input_data.t(), positive_hidden_activations)
+        # Expand the mini-batch
+        activation = wy + self.v_bias.expand_as(wy)
 
-		# Negative phase
-		hidden_activations = positive_hidden_activations
+        # Calculate the probability p_v_given_h
+        p_v_given_h = torch.sigmoid(activation)
 
-		for step in range(self.k):
-			visible_probabilities = self.sample_visible(hidden_activations)
-			hidden_probabilities = self.sample_hidden(visible_probabilities)
-			hidden_activations = (hidden_probabilities >= self._random_probabilities(self.num_hidden)).float()
+        # Bernouilli RBM to predict whether an user loves the movie or not (0 or 1)
+        # This correspnds to whether the n_vis is activated or not activated
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
 
-		negative_visible_probabilities = visible_probabilities
-		negative_hidden_probabilities = hidden_probabilities
+    def train(self, v0, vk, ph0, phk):
 
-		negative_associations = torch.matmul(negative_visible_probabilities.t(), negative_hidden_probabilities)
+        '''
+        Perform contrastive divergence algorithm to optimize the weights that minimize the energy
+        This maximizes the log-likelihood of the model
+        '''
 
-		# Update parameters
-		self.weights_momentum *= self.momentum_coefficient
-		self.weights_momentum += (positive_associations - negative_associations)
+        # Approximate the gradients with the CD algorithm
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
 
-		self.visible_bias_momentum *= self.momentum_coefficient
-		self.visible_bias_momentum += torch.sum(input_data - negative_visible_probabilities, dim=0)
-
-		self.hidden_bias_momentum *= self.momentum_coefficient
-		self.hidden_bias_momentum += torch.sum(positive_hidden_probabilities - negative_hidden_probabilities, dim=0)
-
-		batch_size = input_data.size(0)
-
-		self.weights += self.weights_momentum * self.learning_rate / batch_size
-		self.visible_bias += self.visible_bias_momentum * self.learning_rate / batch_size
-		self.hidden_bias += self.hidden_bias_momentum * self.learning_rate / batch_size
-
-		self.weights -= self.weights * self.weight_decay  # L2 weight decay
-
-		# Compute reconstruction error
-		error = torch.sum((input_data - negative_visible_probabilities)**2)
-
-		return error
-
-	def _sigmoid(self, x):
-		'''Sigmoid function'''
-		return 1 / (1 + torch.exp(-x))
-
-	def _random_probabilities(self, num):
-		'''Generate random probabilities'''
-		random_probabilities = torch.rand(num)
-
-		if self.use_cuda:
-			random_probabilities = random_probabilities.cuda()
-
-		return random_probabilities
-
-class RBMEngine(Engine):
-    """Engine for training & evaluating RBM model"""
-
-	def __init__(self, config):
-		self.model = RBM(config)
-
-		if config['use_cuda'] is True:
-			use_cuda(True, config['device_id'])
-			self.model.cuda()
-
-		super(RBMEngine, self).__init__(config)
-		print(self.model)
+        # Add , 0 for the tensor of 2 dimensions
+        self.v_bias = torch.sum((v0 - vk), 0)
+        self.h_bias = torch.sum((ph0 - phk), 0)
