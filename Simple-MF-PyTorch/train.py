83e485316189d0140f3ec92e2e86e244276e53f5
James Le <le_j6@denison.edu>
2019-11-28 17:19:51 -0500

    Add Loader script

--- Simple-MF-PyTorch/train.py
+++ Simple-MF-PyTorch/train.py
@@ -1,10 +1,22 @@
-# Import Packages
+# Import Python Libraries
 import os.path
 import numpy as np
 import pandas as pd
 import requests
 from zipfile import ZipFile
 from SimpleMF import *
+
+# Import PyTorch Ignite
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.metrics import Loss
+from ignite.metrics import MeanSquaredError
+
+# Import Tensorboard
+from tensorboardX import SummaryWriter
+
+# Import Utility Functions
+from loader import Loader
+from datetime import datetime
 
 # Load preprocessed data
 path = '/Users/khanhnamle/Desktop/CSCI799-Graduate-Independent-Study/Codebase/ml-1m/'
@@ -22,3 +34,15 @@ test_y = fh['test_y']
 # Number of users and number of items
 n_user = int(fh['n_user'])
 n_item = int(fh['n_item'])
+
+# Hyperparameters
+lr = 1e-2
+# Number of dimensions per user, item
+k = 10
+# regularization constant
+c_vector = 1e-6
+
+# Setup logging
+log_dir = 'runs/simple_mf_01_' + str(datetime.now()).replace(' ', '_')
+print(log_dir)
+writer = SummaryWriter(log_dir=log_dir)
