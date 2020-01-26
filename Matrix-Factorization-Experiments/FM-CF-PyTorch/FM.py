18ae28462938f94d8a4512ce5a51b6c7b8979fea
James Le <le_j6@denison.edu>
2020-01-25 21:25:00 -0500

    Rerun Factorization Machines and Mixture of Tastes models

--- Matrix-Factorization-Experiments/FM-CF-PyTorch/FM.py
+++ Matrix-Factorization-Experiments/FM-CF-PyTorch/FM.py
@@ -3,45 +3,60 @@ from torch import nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+
 class MF(nn.Module):
     # Iteration counter
     itr = 0
 
     def __init__(self, n_feat, k=10, c_feat=1.0, c_bias=1.0, writer=None):
-        '''
-        Function to initialize the MF class
-        '''
+        """
+        :param n_feat: Feature column
+        :param k: Dimensions constant
+        :param c_feat: Regularization constant for the features
+        :param c_bias: Regularization constant for the biases
+        :param writer: Log results via TensorBoard
+        """
         super(MF, self).__init__()
 
         # This will hold the logging
         self.writer = writer
 
-        # These are simple hyperparameters
+        # These are the hyper-parameters
         self.k = k
         self.n_feat = n_feat
-        self.feat = nn.Embedding(n_feat, k)
-        self.bias_feat = nn.Embedding(n_feat, 1)
         self.c_feat = c_feat
         self.c_bias = c_bias
 
+        # The embedding matrices for the features and the feature's biases
+        self.feat = nn.Embedding(n_feat, k)
+        self.bias_feat = nn.Embedding(n_feat, 1)
+
     def __call__(self, train_x):
+        """This is the most important function in this script"""
+        # Pull out biases
         biases = index_into(self.bias_feat.weight, train_x).squeeze().sum(dim=1)
-        vectrs = index_into(self.feat.weight, train_x)
-        interx = factorization_machine(vectrs).squeeze().sum(dim=1)
-        logodds = biases + interx
-        return logodds
+
+        # Initialize vector features using the feature weights
+        vector_features = index_into(self.feat.weight, train_x)
+
+        # Use factorization machines to pull out the interactions
+        interactions = factorization_machine(vector_features).squeeze().sum(dim=1)
+
+        # Final prediction is the sum of biases and interactions
+        prediction = biases + interactions
+        return prediction
 
     def loss(self, prediction, target):
-        '''
+        """
         Function to calculate the loss metric
-        '''
-        # MSE error between target = R_ui and prediction = p_u * q_i
+        """
+        # Calculate the Mean Squared Error between target and prediction
         loss_mse = F.mse_loss(prediction.squeeze(), target.squeeze())
 
-        # Add regularization to the features
+        # Compute L2 regularization over feature matrices
         prior_feat = l2_regularize(self.feat.weight) * self.c_feat
 
-        # Add up the MSE loss + feature regularzation
+        # Add the MSE loss and feature regularization to get total loss
         total = (loss_mse + prior_feat)
 
         # This logs all local variables to tensorboard
@@ -50,24 +65,27 @@ class MF(nn.Module):
                 self.writer.add_scalar(name, var, self.itr)
         return total
 
+
 def l2_regularize(array):
-    '''
+    """
     Function to do L2 regularization
-    '''
+    """
     loss = torch.sum(array ** 2.0)
     return loss
+
 
 def index_into(arr, idx):
     new_shape = (idx.size()[0], idx.size()[1], arr.size()[1])
     return arr[idx.resize(torch.numel(idx.data))].view(new_shape)
 
+
 def factorization_machine(v, x=None):
-    # Takes an input 2D matrix v of n vectors, each d-dimensional
-    # Produces output that is d-dimensional
-    # v is (batchsize, n_features, dim)
-    # x is (batchsize, n_features)
-    # x functions as a weight array, assumed to be 1 if missing
-    # Uses Rendle's trick for computing pairs of features in linear time
+    """
+    Takes an input 2D matrix v of n vectors, each d-dimensional
+    :param v: (batchsize, n_features, dim)
+    :param x: (batchsize, n_features) functions as a weight array, assumed to be 1 if missing
+    :return: output that is d-dimensional
+    """
     batchsize = v.size()[0]
     n_features = v.size()[1]
     n_dim = v.size()[2]
@@ -77,6 +95,7 @@ def factorization_machine(v, x=None):
     else:
         x = x.expand(batchsize, n_features, n_dim)
 
-    t0 = (v * x).sum(dim=1)**2.0
-    t1 = (v**2.0 * x**2.0).sum(dim=1)
+    # Uses Rendle's trick for computing pairs of features in linear time
+    t0 = (v * x).sum(dim=1) ** 2.0
+    t1 = (v ** 2.0 * x ** 2.0).sum(dim=1)
     return 0.5 * (t0 - t1)
--- Matrix-Factorization-Experiments/FM-CF-PyTorch/train.py
+++ Matrix-Factorization-Experiments/FM-CF-PyTorch/train.py
@@ -1,6 +1,6 @@
-# Import Python Libraries
+# Import NumPy and PyTorch
 import numpy as np
-import pandas as pd
+import torch
 
 # Import PyTorch Ignite
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
@@ -22,20 +22,21 @@ path = '../../ml-1m/'
 fh = np.load(path + 'dataset.npz')
 
 # We have a bunch of feature columns and last column is the y-target
-# Note pytorch is finicky about need int64 types
+# Note Pytorch is finicky about need int64 types
 train_x = fh['train_x'].astype(np.int64)
 train_y = fh['train_y']
 
-# We've already split into train & test
+# We've already split the data into train & test set
 test_x = fh['test_x'].astype(np.int64)
 test_y = fh['test_y']
 
-# Number of users, number of items, number of occupations, and number of ranks
+# Extract the number of users, the number of items, the number of occupations, and the number of ranks
 n_user = int(fh['n_user'])
 n_item = int(fh['n_item'])
 n_occu = int(fh['n_occu'])
 n_rank = int(fh['n_ranks'])
-# Add them up to get number of features
+
+# Add them up to get the total number of features
 n_feat = n_user + n_item + n_occu + n_rank
 
 train_x[:, 1] += n_user
@@ -45,12 +46,11 @@ test_x[:, 1] += n_user
 test_x[:, 2] += n_user + n_item
 test_x[:, 3] += n_user + n_item + n_occu
 
-# Hyperparameters
-lr = 1e-2 # learning rate
-k = 10 # number of dimensions per user and item
-# New parameter for regularizing bias and features
-c_bias = 1e-6
-c_feat = 1e-6
+# Define the Hyper-parameters
+lr = 1e-2  # Learning rate
+k = 10  # Number of dimensions per user and item
+c_bias = 1e-6  # Bias constant
+c_feat = 1e-6  # Regularization constant
 
 # Setup logging
 log_dir = 'runs/simple_mf_06_fm_' + str(datetime.now()).replace(' ', '_')
@@ -58,42 +58,50 @@ writer = SummaryWriter(log_dir=log_dir)
 
 # Instantiate the model class object
 model = MF(n_feat, k=k, c_bias=c_bias, c_feat=c_feat, writer=writer)
+
+# Use Adam optimizer
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 # Create a supervised trainer
 trainer = create_supervised_trainer(model, optimizer, model.loss)
+
 # Use Mean Squared Error as accuracy metric
 metrics = {'accuracy': MeanSquaredError()}
-# Create a supervised evaluator
-evaluat = create_supervised_evaluator(model, metrics=metrics)
 
-# Load the train and test datasets
+# Create a supervised evaluator
+evaluator = create_supervised_evaluator(model, metrics=metrics)
+
+# Load the train and test data
 train_loader = Loader(train_x, train_y, batchsize=1024)
 test_loader = Loader(test_x, test_y, batchsize=1024)
 
+
 def log_training_loss(engine, log_interval=500):
-    '''
+    """
     Function to log the training loss
-    '''
-    epoch = engine.state.epoch # Keep track of epochs
-    itr = engine.state.iteration # Keep track of iterations
-    fmt = "Epoch[{}] Iteration[{}/{}] Loss: {:.2f}"
-    msg = fmt.format(epoch, itr, len(train_loader), engine.state.output) # Keep track of outputs
-    model.itr = itr
-    if itr % log_interval == 0:
+    """
+    model.itr = engine.state.iteration  # Keep track of iterations
+    if model.itr % log_interval == 0:
+        fmt = "Epoch[{}] Iteration[{}/{}] Loss: {:.2f}"
+        # Keep track of epochs and outputs
+        msg = fmt.format(engine.state.epoch, engine.state.iteration, len(train_loader), engine.state.output)
         print(msg)
+
 
 trainer.add_event_handler(event_name=Events.ITERATION_COMPLETED, handler=log_training_loss)
 
+
 def log_validation_results(engine):
-    '''
+    """
     Function to log the validation results
-    '''
-    evaluat.run(test_loader) # When triggered, run the validation set
-    metrics = evaluat.state.metrics # Keep track of metrics
-    avg_accuracy = metrics['accuracy']
+    """
+    # When triggered, run the validation set
+    evaluator.run(test_loader)
+    # Keep track of accuracy metrics
+    avg_accuracy = evaluator.state.metrics['accuracy']
     print("Epoch[{}] Validation MSE: {:.2f} ".format(engine.state.epoch, avg_accuracy))
     writer.add_scalar("validation/avg_accuracy", avg_accuracy, engine.state.epoch)
+
 
 trainer.add_event_handler(event_name=Events.EPOCH_COMPLETED, handler=log_validation_results)
 
