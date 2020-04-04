b83205a8fea3e783a9a8e098e87d18a7a4e26166
James Le <le_j6@denison.edu>
2020-04-04 16:12:32 -0400

    Add README for AutoRec

---
+++ Autoencoders-Experiments/AutoRec-TensorFlow/README.md
@@ -0,0 +1,33 @@
+# Autoencoders Meet Collaborative Filtering
+
+This is my TensorFlow implementation of the AutoRec model in the paper "[Autoencoders Meet Collaborative Filtering](https://users.cecs.anu.edu.au/~akmenon/papers/autorec/autorec-paper.pdf)" by Suvash Sedhain, Aditya Krishna Menon, Scott Sanner, and Lexing Xie.
+This is one of the first papers that proposes an autoencoder framework for collaborative filtering.
+
+<img src="https://github.com/khanhnamle1994/transfer-rec/blob/master/Autoencoders-Experiments/AutoRec-TensorFlow/AutoRec.png" width="700">
+
+## Requirements
+```
+TensorFlow 1.4
+Python 3.6
+Numpy
+Pandas
+```
+
+## Scripts
+* [data_preprocessor.py](https://github.com/khanhnamle1994/transfer-rec/blob/master/Autoencoders-Experiments/AutoRec-TensorFlow/data_preprocessor.py): This is the script that loads and pre-processes the [MovieLens1M dataset](https://github.com/khanhnamle1994/transfer-rec/tree/master/ml-1m).
+* [AutoRec.py](https://github.com/khanhnamle1994/transfer-rec/blob/master/Autoencoders-Experiments/AutoRec-TensorFlow/AutoRec.py): This is the model script that defines the AutoRec model.
+* [main.py](https://github.com/khanhnamle1994/transfer-rec/blob/master/Autoencoders-Experiments/AutoRec-TensorFlow/main.py): This is the main training script. You can simply run `python main.py` to execute it.
+
+## Results
+The full results are stored in [this folder](https://github.com/khanhnamle1994/transfer-rec/tree/master/Autoencoders-Experiments/AutoRec-TensorFlow/results). I tuned the hyperparameters according to the paper:
+- Activation Function = Sigmoid
+- Learning Rate = 0.001
+- Batch Size = 128
+- Decay the Learning Rate for each 50 epochs
+- Number of Hidden Neurons = 500 (within the Autoencoder)
+- L2 Regularizer Value = 1
+- Optimizer Method = Adam
+- Random Seed = 1000
+- Number of Training Epochs = 100
+
+After training the model for 100 epochs, I got the test loss RMSE = 0.9102.
--- Autoencoders-Experiments/AutoRec-TensorFlow/main.py
+++ Autoencoders-Experiments/AutoRec-TensorFlow/main.py
@@ -3,6 +3,19 @@ from AutoRec import AutoRec
 import tensorflow as tf
 import argparse
 
+'''
+Parser Arguments:
+> Number of hidden neurons
+> L2 regularizer lambda value
+> Number of training epochs
+> Batch size
+> Optimizer method
+> Gradient clip (True or False)
+> Learning rate
+> Decay epoch step
+> Random seed
+> Display step
+'''
 parser = argparse.ArgumentParser(description='I-AutoRec ')
 parser.add_argument('--hidden_neuron', type=int, default=500)
 parser.add_argument('--lambda_value', type=float, default=1)
@@ -22,17 +35,21 @@ args = parser.parse_args()
 tf.random.set_seed(args.random_seed)
 np.random.seed(args.random_seed)
 
+# Info on the MovieLens1M data
 data_name = 'ml-1m'
 num_users = 6040
 num_items = 3952
 num_total_ratings = 1000209
-train_ratio = 0.75
-path = "../../%s" % data_name + "/"
 
+# Data is split into random 75% - 25% train-test sets
+train_ratio = 0.75
+
+# Define the data path directory and result path directory
+path = "../../%s" % data_name + "/"
 result_path = './results/' + str(args.random_seed) + '_' + str(args.optimizer_method) + '_' + \
               str(args.base_lr) + "/"
 
-# Read ratings data
+# Read ratings data with the `read_ratings` function from `data_preprocessor.py`
 R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R, num_train_ratings, num_test_ratings, \
 user_train_set, item_train_set, user_test_set, item_test_set \
     = read_rating(path, num_users, num_items, num_total_ratings, 1, 0, train_ratio)
@@ -43,9 +60,11 @@ config = tf.compat.v1.ConfigProto()
 
 # Run a TensorFlow session
 with tf.compat.v1.Session(config=config) as sess:
+    # Define the AutoRec class from `AutoRec.py`
     AutoRec = AutoRec(sess, args,
                       num_users, num_items,
                       R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R, num_train_ratings, num_test_ratings,
                       user_train_set, item_train_set, user_test_set, item_test_set,
                       result_path)
+    # Run the AutoRec model
     AutoRec.run()
