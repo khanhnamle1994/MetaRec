df87374566813bcd140cd2457d6d711b19fce012
James Le <le_j6@denison.edu>
2020-04-19 11:21:06 -0400

    NeuralCF: Add scripts for loading data, building model, and executing training process

---
+++ Multilayer-Perceptron-Experiments/Neural-CF-PyTorch-Version2/layer.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch
+
+
+class FeaturesEmbedding(torch.nn.Module):
+    """
+    Class to get feature embeddings
+    """
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        torch.nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return self.embedding(x)
+
+
+class MultiLayerPerceptron(torch.nn.Module):
+    """
+    Class to instantiate a Multilayer Perceptron model
+    """
+
+    def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
+        super().__init__()
+        layers = list()
+        for embed_dim in embed_dims:
+            layers.append(torch.nn.Linear(input_dim, embed_dim))
+            layers.append(torch.nn.BatchNorm1d(embed_dim))
+            layers.append(torch.nn.ReLU())
+            layers.append(torch.nn.Dropout(p=dropout))
+            input_dim = embed_dim
+        if output_layer:
+            layers.append(torch.nn.Linear(input_dim, 1))
+        self.mlp = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        """
+        :param x: Float tensor of size ``(batch_size, num_fields, embed_dim)``
+        """
+        return self.mlp(x)
