9a309d5134f365559a67907c8e79ea1a76393cc4
James Le <le_j6@denison.edu>
2020-05-15 08:46:27 -0400

    CDAE: Add data loading and model testing scripts

--- Autoencoders-Experiments/CDAE-TensorFlow/load_data_ranking.py
+++ Autoencoders-Experiments/CDAE-TensorFlow/load_data_ranking.py
@@ -7,7 +7,7 @@ from scipy.sparse import csr_matrix
 
 def load_data_all():
     """
-    Function to load the MovieLens 1M ratings
+    Function to load the MovieLens 1M ratings excluding the negative ratings
     """
     # Read ratings file into a data-frame
     path = "../../ml-1m/ratings.dat"
@@ -16,7 +16,6 @@ def load_data_all():
     # Get the number of users and number of items
     n_users = df.user_id.unique().shape[0]
     n_items = df.item_id.unique().shape[0]
-    print("Number of users:", n_users, "Number of items:", n_items)
 
     # Get the train and test set (80/20 split ratio defaulted)
     train_data, test_data = train_test_split(df, test_size=0.2)
@@ -76,7 +75,59 @@ def load_data_all():
     for u in range(n_users):
         test_dict[u] = test_matrix.getrow(u).nonzero()[1]
 
+    print("Load data finished. Number of users:", n_users, "Number of items:", n_items)
+
     return train_interaction_matrix, test_dict, n_users, n_items
 
 
-load_data_all()
+def load_data_neg():
+    """
+    Function to load all the MovieLens 1M ratings including the negative ratings
+    """
+    # Read ratings file into a data-frame
+    path = "../../ml-1m/ratings.dat"
+    df = pd.read_csv(path, delimiter='::', names=['user_id', 'item_id', 'rating', 'time'], engine='python')
+
+    # Get the number of users and number of items
+    n_users = df.user_id.unique().shape[0]
+    n_items = df.item_id.unique().shape[0]
+
+    # Get the train and test set (80/20 split ratio defaulted)
+    train_data, test_data = train_test_split(df, test_size=0.2)
+    train_data = pd.DataFrame(train_data)
+    test_data = pd.DataFrame(test_data)
+
+    # Empty arrays to store the rows, columns, and targets from train set
+    train_row = []
+    train_col = []
+    train_rating = []
+    # Populate train_row, train_col, and train_rating
+    for line in train_data.itertuples():
+        u = line[1] - 1
+        i = line[2] - 1
+        train_row.append(u)
+        train_col.append(i)
+        train_rating.append(1)
+    # Use a Compressed Sparse Row matrix to represent the train set
+    train_matrix = csr_matrix((train_rating, (train_row, train_col)), shape=(n_users, n_items))
+
+    # Empty arrays to store the rows, columns, and targets from test set
+    test_row = []
+    test_col = []
+    test_rating = []
+    # Populate test_row, test_col, and test_rating
+    for line in test_data.itertuples():
+        test_row.append(line[1] - 1)
+        test_col.append(line[2] - 1)
+        test_rating.append(1)
+    # Use a Compressed Sparse Row matrix to represent the test set
+    test_matrix = csr_matrix((test_rating, (test_row, test_col)), shape=(n_users, n_items))
+
+    # Empty dictionary to store test set
+    test_dict = {}
+    for u in range(n_users):
+        test_dict[u] = test_matrix.getrow(u).nonzero()[1]
+
+    print("Load data finished. Number of users:", n_users, "Number of items:", n_items)
+
+    return train_matrix.todok(), test_dict, n_users, n_items
