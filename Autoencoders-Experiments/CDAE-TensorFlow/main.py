40e59c27c2e08d01831678f8296324c642a9a04c
James Le <le_j6@denison.edu>
2020-05-15 09:20:52 -0400

    CDAE: Add CDAE model definition script + Rename other scripts

--- Autoencoders-Experiments/CDAE-TensorFlow/main.py
+++ Autoencoders-Experiments/CDAE-TensorFlow/main.py
@@ -1,15 +1,14 @@
 import argparse
-from cdae import ICDAE
-
-from load_data_ranking import load_data_neg, load_data_all
+from model import CDAE
+from load_data import load_data_neg, load_data_all
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Collaborative Denoising Auto-Encoder')
     parser.add_argument('--model', default='CDAE')
-    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--epochs', type=int, default=1000)
     parser.add_argument('--num_factors', type=int, default=10)
-    parser.add_argument('--display_step', type=int, default=10)
+    parser.add_argument('--display_step', type=int, default=1000)
     parser.add_argument('--batch_size', type=int, default=1024)  # 128 for unlimpair
     parser.add_argument('--learning_rate', type=float, default=1e-3)  # 1e-4 for unlimpair
     parser.add_argument('--reg_rate', type=float, default=0.1)  # 0.01 for unlimpair
@@ -31,7 +30,7 @@ if __name__ == '__main__':
     # Model selection
     if args.model == "CDAE":
         train_data, test_data, n_user, n_item = load_data_all(test_size=0.2, sep="\t")
-        model = ICDAE(n_user, n_item)
+        model = CDAE(n_user, n_item)
     # build and execute the model
     if model is not None:
         model.build_network()
