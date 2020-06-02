2f53acdd87d211010657571a0385d3eef217a83f
James Le <le_j6@denison.edu>
2020-06-02 09:51:21 -0400

    Add Matrix Factorization code and algorithm

---
+++ Django-Web-Service/research/MF.py
@@ -0,0 +1,69 @@
+# Import PyTorch Packages
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+# Define the MF Model
+class MF(nn.Module):
+    # Iteration counter
+    itr = 0
+
+    def __init__(self, n_user, n_item, k=10, c_vector=1.0):
+        """
+        :param n_user: User column
+        :param n_item: Item column
+        :param k: Dimensions constant
+        :param c_vector: Regularization constant
+        """
+        super(MF, self).__init__()
+
+        # These are the hyper-parameters
+        self.k = k
+        self.n_user = n_user
+        self.n_item = n_item
+        self.c_vector = c_vector
+
+        # The embedding matrices for user and item are learned and fit by PyTorch
+        self.user = nn.Embedding(n_user, k)
+        self.item = nn.Embedding(n_item, k)
+
+    def __call__(self, train_x):
+        """This is the most important function in this script"""
+        # These are the user indices, and correspond to "u" variable
+        user_id = train_x[:, 0]
+        # These are the item indices, correspond to the "i" variable
+        item_id = train_x[:, 1]
+
+        # Initialize a vector user = p_u using the user indices
+        vector_user = self.user(user_id)
+        # Initialize a vector item = q_i using the item indices
+        vector_item = self.item(item_id)
+
+        # The user-item interaction: p_u * q_i is a dot product between the 2 vectors above
+        ui_interaction = torch.sum(vector_user * vector_item, dim=1)
+        return ui_interaction
+
+    def loss(self, prediction, target):
+        """
+        Function to calculate the loss metric
+        """
+        # Calculate the Mean Squared Error between target = R_ui and prediction = p_u * q_i
+        loss_mse = F.mse_loss(prediction, target.squeeze())
+
+        # Compute L2 regularization over user (P) and item (Q) matrices
+        prior_user = l2_regularize(self.user.weight) * self.c_vector
+        prior_item = l2_regularize(self.item.weight) * self.c_vector
+
+        # Add up the MSE loss + user & item regularization
+        total = loss_mse + prior_user + prior_item
+
+        return total
+
+
+def l2_regularize(array):
+    """
+    Function to do L2 regularization
+    """
+    loss = torch.sum(array ** 2.0)
+    return loss
---
+++ Django-Web-Service/research/train_MF_recommender.py
@@ -0,0 +1,90 @@
+# Import NumPy and PyTorch
+import numpy as np
+import torch
+
+# Import joblib to save algorithm objects
+import joblib
+
+# Import PyTorch Ignite
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.metrics import MeanSquaredError
+
+# Import Utility Functions
+from loader import Loader
+from MF import MF
+
+# Load preprocessed data
+path = '../../ml-1m/'
+full_data = np.load(path + 'dataset.npz')
+
+# We have a bunch of feature columns and last column is the y-target
+# Note that Pytorch is finicky about need int64 types
+train_x = full_data['train_x'].astype(np.int64)
+train_y = full_data['train_y']
+
+# We've already split the data into train & test set
+test_x = full_data['test_x'].astype(np.int64)
+test_y = full_data['test_y']
+
+# Extract the number of users and number of items
+n_user = int(full_data['n_user'])
+n_item = int(full_data['n_item'])
+
+# Define the Hyper-parameters
+lr = 1e-2  # Learning Rate
+k = 10  # Number of dimensions per user, item
+c_vector = 1e-6  # regularization constant
+
+# Instantiate the MF class object
+model = MF(n_user, n_item, k=k, c_vector=c_vector)
+
+# Use Adam optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+# Create a supervised trainer
+trainer = create_supervised_trainer(model, optimizer, model.loss)
+
+# Use Mean Squared Error as evaluation metric
+metrics = {'evaluation': MeanSquaredError()}
+
+# Create a supervised evaluator
+evaluator = create_supervised_evaluator(model, metrics=metrics)
+
+# Load the train and test data
+train_loader = Loader(train_x, train_y, batchsize=1024)
+test_loader = Loader(test_x, test_y, batchsize=1024)
+
+
+def log_training_loss(engine, log_interval=500):
+    """
+    Function to log the training loss
+    """
+    model.itr = engine.state.iteration  # Keep track of iterations
+    if model.itr % log_interval == 0:
+        fmt = "Epoch[{}] Iteration[{}/{}] Loss: {:.2f}"
+        # Keep track of epochs and outputs
+        msg = fmt.format(engine.state.epoch, engine.state.iteration, len(train_loader), engine.state.output)
+        print(msg)
+
+
+trainer.add_event_handler(event_name=Events.ITERATION_COMPLETED, handler=log_training_loss)
+
+
+def log_validation_results(engine):
+    """
+    Function to log the validation loss
+    """
+    # When triggered, run the validation set
+    evaluator.run(test_loader)
+    # Keep track of the evaluation metrics
+    avg_loss = evaluator.state.metrics['evaluation']
+    print("Epoch[{}] Validation MSE: {:.2f} ".format(engine.state.epoch, avg_loss))
+
+
+trainer.add_event_handler(event_name=Events.EPOCH_COMPLETED, handler=log_validation_results)
+
+# Run the model for 50 epochs
+trainer.run(train_loader, max_epochs=100)
+
+# Save MF algorithm
+joblib.dump(model.state_dict(), "./vanilla_mf.joblib", compress=True)
