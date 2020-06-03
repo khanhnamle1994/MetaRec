6b848b84375dbc30d0d016dc3c0cb0b7f65a2d8e
James Le <le_j6@denison.edu>
2020-06-03 11:09:38 -0400

    VAE: Add Multi-DAE + comment on metric and models cripts

--- Autoencoders-Experiments/VAE-CF-PyTorch-Version1/models.py
+++ Autoencoders-Experiments/VAE-CF-PyTorch-Version1/models.py
@@ -1,7 +1,68 @@
+# Import packages
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
 import numpy as np
+
+
+class MultiDAE(nn.Module):
+    """
+    Container module for Multi-DAE.
+    Multi-DAE : Denoising Autoencoder with Multinomial Likelihood
+    See Variational Autoencoders for Collaborative Filtering
+    https://arxiv.org/abs/1802.05814
+    """
+
+    def __init__(self, p_dims, q_dims=None, dropout=0.5):
+        """
+        Initialize the Multi-DAE architecture class
+        :param p_dims: dimension of the p-network
+        :param q_dims: dimension of the q-network
+        :param dropout: dropout rate
+        """
+        super(MultiDAE, self).__init__()
+        self.p_dims = p_dims
+        if q_dims:
+            assert q_dims[0] == p_dims[-1], "In and Out dimensions must equal to each other"
+            assert q_dims[-1] == p_dims[0], "Latent dimension for p- and q- network mismatches."
+            self.q_dims = q_dims
+        else:
+            self.q_dims = p_dims[::-1]
+
+        self.dims = self.q_dims + self.p_dims[1:]
+        self.layers = nn.ModuleList([nn.Linear(d_in, d_out) for
+                                     d_in, d_out in zip(self.dims[:-1], self.dims[1:])])
+        self.drop = nn.Dropout(dropout)
+
+        self.init_weights()
+
+    def forward(self, input):
+        """
+        Perform a forward pass
+        """
+        h = F.normalize(input)
+        h = self.drop(h)
+
+        for i, layer in enumerate(self.layers):
+            h = layer(h)
+            if i != len(self.weights) - 1:
+                h = F.tanh(h)
+        return h
+
+    def init_weights(self):
+        """
+        Initialize the network weights
+        """
+        for layer in self.layers:
+            # Xavier Initialization for weights
+            size = layer.weight.size()
+            fan_out = size[0]
+            fan_in = size[1]
+            std = np.sqrt(2.0 / (fan_in + fan_out))
+            layer.weight.data.normal_(0.0, std)
+
+            # Normal Initialization for Biases
+            layer.bias.data.normal_(0.0, 0.001)
 
 
 class MultiVAE(nn.Module):
@@ -13,6 +74,12 @@ class MultiVAE(nn.Module):
     """
 
     def __init__(self, p_dims, q_dims=None, dropout=0.5):
+        """
+        Initialize the Multi-DAE architecture class
+        :param p_dims: dimension of the p-network
+        :param q_dims: dimension of the q-network
+        :param dropout: dropout rate
+        """
         super(MultiVAE, self).__init__()
         self.p_dims = p_dims
         if q_dims:
@@ -33,11 +100,17 @@ class MultiVAE(nn.Module):
         self.init_weights()
 
     def forward(self, input):
+        """
+        Perform a forward pass
+        """
         mu, logvar = self.encode(input)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
     def encode(self, input):
+        """
+        Define the encoder module
+        """
         h = F.normalize(input)
         h = self.drop(h)
 
@@ -51,6 +124,9 @@ class MultiVAE(nn.Module):
         return mu, logvar
 
     def reparameterize(self, mu, logvar):
+        """
+        Perform the re-parameterization trick
+        """
         if self.training:
             std = torch.exp(0.5 * logvar)
             eps = torch.randn_like(std)
@@ -59,6 +135,9 @@ class MultiVAE(nn.Module):
             return mu
 
     def decode(self, z):
+        """
+        Define the decoder module
+        """
         h = z
         for i, layer in enumerate(self.p_layers):
             h = layer(h)
@@ -67,6 +146,9 @@ class MultiVAE(nn.Module):
         return h
 
     def init_weights(self):
+        """
+        Initialize the network weights
+        """
         for layer in self.q_layers:
             # Xavier Initialization for weights
             size = layer.weight.size()
@@ -90,10 +172,19 @@ class MultiVAE(nn.Module):
             layer.bias.data.normal_(0.0, 0.001)
 
 
-# Define loss function
 def loss_function(recon_x, x, mu, logvar, anneal=1.0):
+    """
+    Define loss function
+    :param recon_x: reconstructed input
+    :param x: input
+    :param mu: mean of the data distribution
+    :param logvar: variance of the data distribution
+    :param anneal: annealing rate
+    :return: multinomial likelihood loss
+    """
     # BCE = F.binary_cross_entropy(recon_x, x)
     BCE = -torch.mean(torch.sum(F.log_softmax(recon_x, 1) * x, -1))
     KLD = -0.5 * torch.mean(torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1))
 
+    # Final loss is the sum of binary-cross entropy loss and KL-Divergence loss with annealing
     return BCE + anneal * KLD
