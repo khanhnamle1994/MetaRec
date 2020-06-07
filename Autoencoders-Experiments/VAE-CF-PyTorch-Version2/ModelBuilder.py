3a8e1f3ac32f6767c811ee5ecf637d83aa2e897c
James Le <le_j6@denison.edu>
2020-06-07 13:17:06 -0400

    VAE: Add ModelBuilder script

---
+++ Autoencoders-Experiments/VAE-CF-PyTorch-Version2/ModelBuilder.py
@@ -0,0 +1,20 @@
+# Import model
+from MultVAE import MultVAE
+
+
+def build_model(model_name, model_conf, num_users, num_items, device):
+    """
+    Function to build the model
+    :param model_name: choice of model
+    :param model_conf: model configuration
+    :param num_users: number of users
+    :param num_items: number of items
+    :param device: choice of device
+    :return: the model
+    """
+    if model_name == 'MultVAE':
+        model = MultVAE(model_conf, num_users, num_items, device)
+    else:
+        raise NotImplementedError('Choose correct model name.')
+
+    return model
