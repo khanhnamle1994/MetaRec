1ad990542192e89bb2b7185257bbd9b861fab41e
James Le <le_j6@denison.edu>
2020-06-22 10:48:17 -0400

    DeepRec: Incorporate CometML + Update README

--- Autoencoders-Experiments/NVIDIA-DeepRec-TensorFlow/README.md
+++ Autoencoders-Experiments/NVIDIA-DeepRec-TensorFlow/README.md
@@ -3,7 +3,7 @@
 This is my TensorFlow implementation of the DeepRec model in the paper "[Training Deep AutoEncoders for Collaborative Filtering](https://arxiv.org/abs/1708.01715)" by Oleksii Kuchaiev and Boris Ginsburg at NVIDIA.
 This paper continued the [AutoRec](https://github.com/khanhnamle1994/transfer-rec/tree/master/Autoencoders-Experiments/AutoRec-TensorFlow) idea to deepen Autoencoder.
 
-- It uses Masked Mean Squared Error as loss function, same as AutoRec.
+- It uses Masked Root Mean Squared Error as loss function, same as AutoRec.
 - It proposes activation functions with non-zero negative part and unbounded positive part works better.
 - It uses dropout layers after the latent layer to avoid overfitting.
 - It also shows that using large dropout rate after the latent layer allows it to learn robust representations.
@@ -32,11 +32,11 @@ I tuned the hyperparameters according to the paper:
 - Autoencoder architecture layers = [512, 512, 1024, 512, 512] (encoder layer of size (512, 512, 1024), coding layer of size 1024, decoder layer of size (512, 512, n))
 - Activation Function = SELU (scaled exponential linear units)
 - Learning Rate = 0.001
-- Batch Size = 128
+- Batch Size = 512
 - L2 Regularizer (Lambda) Value = 0.001
 - Optimizer Method = Stochastic Gradient Descent
 - Momentum = 0.9
 - Dropout Rate = 0.8
-- Number of Training Epochs = 100
 
-After training the model for 100 epochs, I got the **Test Masked RMSE = 0.9358**.
+After training the model for 500 epochs, I got the **Test Masked RMSE = 0.9310**.
+I also logged the experiment run on CometML, which can be accessed [here](https://www.comet.ml/khanhnamle1994/autoencoders-movielens1m/79db0882636549b5bd8a8d66563602f3).
--- Autoencoders-Experiments/NVIDIA-DeepRec-TensorFlow/main.py
+++ Autoencoders-Experiments/NVIDIA-DeepRec-TensorFlow/main.py
@@ -1,13 +1,20 @@
+# CometML for experiment logging
+from comet_ml import Experiment
+
 # Import packages
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import EarlyStopping
 
 # Import utility scripts
 from data_processor import preprocess_data
 from model import Deep_AE_model
 from utils import show_error, show_rmse, masked_rmse, masked_rmse_clip
+
+# Create an experiment with API key
+experiment = Experiment(api_key="GaicgDHvizDRCbpq2wVV8NHnX", project_name="autoencoders-movielens1M")
 
 # Load the ratings data
 df = pd.read_csv('data/ml1m_ratings.csv', sep='\t', encoding='latin-1',
@@ -19,9 +26,9 @@ num_movies = df['movie_emb_id'].unique().max() + 1
 
 # Split the train (80%), validation (10%), and test (10%) sets
 train_df, test_df = train_test_split(df, stratify=df['user_emb_id'],
-                                     test_size=0.1, random_state=999613182)
+                                     test_size=0.1, random_state=1994)
 train_df, validate_df = train_test_split(train_df, stratify=train_df['user_emb_id'],
-                                         test_size=0.1, random_state=999613182)
+                                         test_size=0.1, random_state=1994)
 
 # Create sparse pivot tables with users in rows and items in columns
 users_items_matrix_train_zero = preprocess_data(train_df, num_users, num_movies, 0)
@@ -57,15 +64,20 @@ regularizer_decode = 0.001
 Deep_AE = Deep_AE_model(users_items_matrix_train_zero,
                         layers, activation, last_activation, dropout,
                         regularizer_encode, regularizer_decode)
+
 # Compile model
 Deep_AE.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss=masked_rmse, metrics=[masked_rmse_clip])
-# Get model summary
-Deep_AE.summary()
-# Train the model and evaluate on validation set
-hist_Deep_AE = Deep_AE.fit(
-    x=users_items_matrix_train_zero, y=users_items_matrix_train_zero,
-    epochs=100, batch_size=128,
-    validation_data=[users_items_matrix_train_zero, users_items_matrix_validate], verbose=2)
+
+# Print model summary to preserve automatically in CometML dashboard
+print(Deep_AE.summary())
+
+# Train the model, evaluate on validation set, and log metrics with the prefix 'train_'
+with experiment.train():
+    hist_Deep_AE = Deep_AE.fit(
+        x=users_items_matrix_train_zero, y=users_items_matrix_train_zero, epochs=500, batch_size=512,
+        validation_data=[users_items_matrix_train_zero, users_items_matrix_validate], verbose=2,
+        callbacks=[EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=3, verbose=2, mode='auto')]
+    )
 
 # Show model loss
 show_error(hist_Deep_AE, 10)
@@ -74,4 +86,28 @@ show_rmse(hist_Deep_AE, 10)
 
 # Predict the model on test set
 predict_deep = Deep_AE.predict(users_items_matrix_train_zero)
-test_result_deep = Deep_AE.evaluate(users_items_matrix_train_zero, users_items_matrix_test)
+
+# Evaluate the model on test set and log metrics with the prefix 'test_'
+with experiment.test():
+    loss, test_result_deep = Deep_AE.evaluate(users_items_matrix_train_zero, users_items_matrix_test)
+    metrics = {
+        'masked_rmse_loss': loss,
+        'masked_rmse_test': test_result_deep
+    }
+    experiment.log_metrics(metrics)
+
+# Dictionary to store model hyper-parameters
+hyper_params = {
+    "layers": "[512, 512, 1024, 512, 512]",
+    "dropout": 0.8,
+    "epochs": 500,
+    "batch_size": 512,
+    "optimizer": "Stochastic Gradient Descent",
+    "activation": "SELU",
+    "learning_rate": 0.001,
+    "momentum": 0.9,
+    "regularizer_value": 0.001
+}
+
+# Log model hyper-parameters
+experiment.log_parameters(hyper_params)
