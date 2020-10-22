0ae9d18f6cdf645a09c92a689f5e321880232c32
James Le <le_j6@denison.edu>
2020-10-22 09:54:40 -0400

    MAMO: Add configuration script

---
+++ Meta-Learning/MAMO/configs.py
@@ -0,0 +1,9 @@
+# Configuration Settings
+config_settings = {
+    'rand': True, 'random_state': 100, 'split_ratio': 0.8,
+    'support_size': 15, 'query_size': 5, 'embedding_dim': 100,
+    'n_layer': 2, 'alpha': 0.5, 'beta': 0.05, 'gamma': 0.1,
+    'rho': 0.01, 'lambda': 0.05, 'tao': 0.01, 'n_k': 3,
+    'batch_size': 5, 'n_epoch': 3, 'n_inner_loop': 3,
+    'activation_function': 'leaky_relu'
+}
